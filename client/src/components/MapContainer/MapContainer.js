225ddab13740c45eca6253c73714518897d06690
Tahmid <hossaint216@gmail.com>
2022-04-07 00:12:11 +0600

    Some Changes

--- client/src/components/MapContainer/MapContainer.js
+++ client/src/components/MapContainer/MapContainer.js
@@ -16,7 +16,7 @@ import { logOut } from '../../actions/users';
 import { Avatar } from '@material-ui/core';
 import LikePins from '../LikePins/LikePins';
 import mapboxgl from 'mapbox-gl/dist/mapbox-gl-csp';
-import MapboxWorker from 'worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker'; // Load worker code separately with worker-loader
+import MapboxWorker from 'mapbox-gl/dist/mapbox-gl-csp-worker';
 
 mapboxgl.workerClass = MapboxWorker;
 
